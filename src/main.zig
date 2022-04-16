14e982954b841657d41c9b1ff22392376fbe3f2c
Daniel Mironow <sl7@flate.io>
2022-04-16 21:30:54 +0200

    added bubble sort

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,14 @@
 const std = @import("std");
 const fac = @import("faculty.zig");
+const bubble = @import("bubble_sort.zig");
 const print = std.debug.print;
 
 pub fn main() anyerror!void {
-    print("faculty(1): {d}\n", .{fac.faculty(1)});
-    print("faculty(2): {d}\n", .{fac.faculty(2)});
-    print("faculty(3): {d}\n", .{fac.faculty(3)});
-    print("faculty(4): {d}\n", .{fac.faculty(4)});
-    print("faculty(5): {d}\n", .{fac.faculty(5)});
+    var arr1 = [_]i32{ 5, 4, 3, 2, 1 };
+
+    bubble.bubble_sort(&arr1);
+
+    for (arr1) |e| {
+        print("{d}\n", .{e});
+    }
 }
