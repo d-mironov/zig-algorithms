595276aa397146596bad23fdb6c82fe463002cde
Daniel Mironow <sl7@flate.io>
2022-04-16 23:23:37 +0200

    added optimized bubble sort, selection sort

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,19 @@
 const std = @import("std");
 const fac = @import("faculty.zig");
-const bubble = @import("bubble_sort.zig");
+const sorting = @import("sorting.zig");
 const print = std.debug.print;
 
 pub fn main() anyerror!void {
     var arr1 = [_]i32{ 5, 4, 3, 2, 1 };
+    var arr2 = [_]i32{ 5, 4, 3, 2, 1 };
+    var arr3 = [_]i32{ 5, 4, 3, 2, 1 };
 
-    bubble.bubble_sort(&arr1);
+    sorting.bubble_sort(&arr1);
+    sorting.print_arr(&arr1);
 
-    for (arr1) |e| {
-        print("{d}\n", .{e});
-    }
+    sorting.bubble_sort_optimized(&arr2);
+    sorting.print_arr(&arr2);
+
+    sorting.selection_sort(&arr3);
+    sorting.print_arr(&arr3);
 }
---
+++ src/sorting.zig
@@ -0,0 +1,56 @@
+const std = @import("std");
+const print = std.debug.print;
+
+fn swap(a: *i32, b: *i32) void {
+    const tmp = a.*;
+    a.* = b.*;
+    b.* = tmp;
+}
+
+pub fn print_arr(arr: []i32) void {
+    for (arr) |e| {
+        print("| {d} ", .{e});
+    }
+    print("|\n", .{});
+}
+
+pub fn bubble_sort(arr: []i32) void {
+    var step: u32 = 0;
+    while (step < arr.len) : (step += 1) {
+        var i: u32 = 0;
+        while (i < arr.len - step - 1) : (i += 1) {
+            if (arr[i] > arr[i + 1]) {
+                swap(&arr[i], &arr[i + 1]);
+            }
+        }
+    }
+}
+
+pub fn bubble_sort_optimized(arr: []i32) void {
+    var step: u32 = 0;
+    while (step < arr.len) : (step += 1) {
+        var swapped: bool = false;
+        var i: usize = 0;
+        while (i < arr.len - step - 1) : (i += 1) {
+            swap(&arr[i], &arr[i + 1]);
+            swapped = true;
+        }
+        if (swapped == false) {
+            break;
+        }
+    }
+}
+
+pub fn selection_sort(arr: []i32) void {
+    var step: u32 = 0;
+    while (step < arr.len) : (step += 1) {
+        var min_index = step;
+        var i = step + 1;
+        while (i < arr.len) : (i += 1) {
+            if (arr[i] < arr[min_index]) {
+                min_index = i;
+            }
+        }
+        swap(&arr[min_index], &arr[step]);
+    }
+}
