459de486e6aabb2825bd133e8d37dff44f72bd03
Daniel Mironow <sl7@flate.io>
2022-04-19 11:03:13 +0200

    testing in main.zig

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const fac = @import("faculty.zig");
-const sorting = @import("sorting.zig");
+const sort = @import("sort.zig");
+const search = @import("search.zig");
 const print = std.debug.print;
 
 pub fn main() anyerror!void {
@@ -8,12 +9,18 @@ pub fn main() anyerror!void {
     var arr2 = [_]i32{ 5, 4, 3, 2, 1 };
     var arr3 = [_]i32{ 5, 4, 3, 2, 1 };
 
-    sorting.bubble_sort(&arr1);
-    sorting.print_arr(&arr1);
+    sort.bubble(&arr1);
+    sort.print_arr(&arr1);
 
-    sorting.bubble_sort_optimized(&arr2);
-    sorting.print_arr(&arr2);
+    sort.bubble_sort_optimized(&arr2);
+    sort.print_arr(&arr2);
 
-    sorting.selection_sort(&arr3);
-    sorting.print_arr(&arr3);
+    sort.selection(&arr3);
+    sort.print_arr(&arr3);
+
+    var arr4 = [_]i32{ 1, 2, 3, 4, 5, 6, 7, 8 };
+    print("arr search 2: {d}\n", .{try search.binary(&arr4, 2)});
+    print("arr search 7: {d}\n", .{try search.binary(&arr4, 7)});
+    print("arr linear search 7: {d}\n", .{try search.linear(&arr4, 7)});
+    //print("arr linear search 10: {d}\n", .{try search.linear(&arr4, 10)});
 }
---
+++ src/sort.zig
@@ -0,0 +1,69 @@
+const std = @import("std");
+const print = std.debug.print;
+
+fn swap(a: *i32, b: *i32) void {
+    const tmp = a.*;
+    a.* = b.*;
+    b.* = tmp;
+}
+
+pub fn print_arr(arr: []i32) void {
+    for (arr) |e| {
+        print("| {d} ", .{e});
+    }
+    print("|\n", .{});
+}
+
+pub fn bubble(arr: []i32) void {
+    var step: u32 = 0;
+    while (step < arr.len) : (step += 1) {
+        var i: u32 = 0;
+        while (i < arr.len - step - 1) : (i += 1) {
+            if (arr[i] > arr[i + 1]) {
+                swap(&arr[i], &arr[i + 1]);
+            }
+        }
+    }
+}
+
+pub fn bubble_sort_optimized(arr: []i32) void {
+    var step: u32 = 0;
+    while (step < arr.len) : (step += 1) {
+        var swapped: bool = false;
+        var i: usize = 0;
+        while (i < arr.len - step - 1) : (i += 1) {
+            swap(&arr[i], &arr[i + 1]);
+            swapped = true;
+        }
+        if (swapped == false) {
+            break;
+        }
+    }
+}
+
+pub fn selection(arr: []i32) void {
+    var step: u32 = 0;
+    while (step < arr.len) : (step += 1) {
+        var min_index = step;
+        var i = step + 1;
+        while (i < arr.len) : (i += 1) {
+            if (arr[i] < arr[min_index]) {
+                min_index = i;
+            }
+        }
+        swap(&arr[min_index], &arr[step]);
+    }
+}
+
+pub fn insertion(arr: []i32) void {
+    var step: u32 = 1;
+    while (step < arr.len) : (step += 1) {
+        var k = arr[step];
+        var i: u32 = step - 1;
+        while (k < arr[i] and i > 0) : (i -= 1) {
+            print("{d}\n", .{i});
+            arr[i + 1] = arr[i];
+        }
+        arr[i + 1] = k;
+    }
+}
