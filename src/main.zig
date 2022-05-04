4a444cbb04780112abe195d155981ed27ac16a53
Daniel Mironow <sl7@flate.io>
2022-05-04 21:52:08 +0200

    added docs to sorting algorithms

--- src/main.zig
+++ src/main.zig
@@ -3,18 +3,21 @@ const sort = @import("sort.zig");
 const search = @import("search.zig");
 const math = @import("algo_math.zig");
 const print = std.debug.print;
-const rng = std.rand.Random;
 
 pub fn main() anyerror!void {
     var arr1 = [_]i32{ 76, 1, 35, 75, 56, 7, 8, 96, 54, 53 };
-    sort.quicksort(&arr1);
+    sort.quicksort(&arr1, true);
     sort.print_arr(&arr1);
 
     var arr2 = [_]i32{ 5, 5, 5, 5 };
-    sort.quicksort(&arr2);
+    sort.quicksort(&arr2, true);
     sort.print_arr(&arr2);
 
     var arr3 = [_]i32{ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-    sort.quicksort(&arr3);
+    sort.quicksort(&arr3, true);
     sort.print_arr(&arr3);
+
+    var arr4 = [_]i32{};
+    sort.quicksort(&arr4, true);
+    sort.print_arr(&arr4);
 }
--- src/sort.zig
+++ src/sort.zig
@@ -2,12 +2,29 @@ const std = @import("std");
 const print = std.debug.print;
 const rng = std.rand.DefaultPrng;
 
+/// Swap function
+///
+/// Swap two variables by their values
+///
+/// Arguments:
+/// > `a`, `b`: Variables to swap
+///
+/// Return:
+/// > `void`
+/// > 
 fn swap(a: *i32, b: *i32) void {
     const tmp = a.*;
     a.* = b.*;
     b.* = tmp;
 }
 
+/// Print the specified array 
+/// in the form:
+/// > `| x0 | x1 | ... | xn |`
+///
+/// Arguments:
+/// > `arr`: Array to print
+///
 pub fn print_arr(arr: []i32) void {
     for (arr) |e| {
         print("| {d} ", .{e});
@@ -15,6 +32,18 @@ pub fn print_arr(arr: []i32) void {
     print("|\n", .{});
 }
 
+/// # BubbleSort
+/// ------
+/// Description:
+/// Perform the bubble sort algorithm on an `i32` array
+/// ------
+/// Arguments:
+/// > `arr`: `i32` array to perform BubbleSort on
+/// ------
+/// TODO: 
+/// > [] add descending option
+/// ------
+///
 pub fn bubble(arr: []i32) void {
     var step: u32 = 0;
     while (step < arr.len) : (step += 1) {
@@ -27,6 +56,19 @@ pub fn bubble(arr: []i32) void {
     }
 }
 
+/// # BubbleSort Optimized
+/// ------
+/// Description:
+/// Perform the bubble sort algorithm on an `i32` array.
+/// This is an optimized version.
+/// ------
+/// Arguments:
+/// > `arr`: `i32` array to perform BubbleSort on
+/// ------
+/// TODO: 
+/// > [] add descending option
+/// ------
+///
 pub fn bubble_sort_optimized(arr: []i32) void {
     var step: u32 = 0;
     while (step < arr.len) : (step += 1) {
@@ -42,6 +84,18 @@ pub fn bubble_sort_optimized(arr: []i32) void {
     }
 }
 
+/// # SelectionSort
+/// ------
+/// Description:
+/// Perform the SelectionSort algorithm on an `i32` array.
+/// ------
+/// Arguments:
+/// > `arr`: `i32` array to perform SelectionSort on
+/// ------
+/// TODO: 
+/// > [] add descending option
+/// ------
+///
 pub fn selection(arr: []i32) void {
     var step: u32 = 0;
     while (step < arr.len) : (step += 1) {
@@ -69,10 +123,31 @@ pub fn insertion(arr: []i32) void {
     }
 }
 
-pub fn partition(arr: []i32, left: i32, right: i32) i32 {
+/// # QuickSort Partition helper
+/// -------------------------------------------------------------------------
+/// this function helps partitioning the array 
+/// to have the smallest numbers on the left and the 
+/// greatest numbers on the right side, all depending on 
+/// a **pivot** point.
+/// Pivot can either be just the first element in the array,
+/// or be selected random from the array by setting `randomize` to `true`.
+/// -------------------------------------------------------------------------
+/// Arguments:
+/// > `arr`: Array to partition
+/// > `left`: lower end where to start partitioning
+/// > `right`: upper end where to end partitioning
+/// > `randomize`: randomized pivot select if `true`, else first element from 
+/// list
+/// -------------------------------------------------------------------------
+/// Return:
+/// > `i32`: position of the pivot point -> useful for further partitioning.
+///
+pub fn partition(arr: []i32, left: i32, right: i32, randomize: bool) i32 {
     var low = @intCast(u32, left);
     var high = @intCast(u32, right);
-    var pivot = arr[low];
+
+    var rnd = rng.init(0);
+    var pivot = arr[if (randomize == true) rnd.random().intRangeAtMost(u32, low, high) else low];
 
     if (arr.len == 0) {
         return 0;
@@ -94,14 +169,46 @@ pub fn partition(arr: []i32, left: i32, right: i32) i32 {
     return @intCast(i32, low);
 }
 
-pub fn quicksort_recursive(arr: []i32, left: i32, right: i32) void {
+/// # QuickSort Recursive
+/// -------------------------------------------------------------------------
+/// Description:
+/// Sorts the array with QuickSort from lower(`left`) to upper(`right`) limit.
+/// -------------------------------------------------------------------------
+/// Arguments:
+/// > `arr`: Array to sort
+/// > `left`: lower end where to start sorting
+/// > `right`: upper end where to end sorting
+/// > `randomize`: randomized pivot select if `true`, else first element from 
+/// list
+/// -------------------------------------------------------------------------
+/// Return:
+/// > `void`
+///
+pub fn quicksort_recursive(arr: []i32, left: i32, right: i32, randomize: bool) void {
     if (left < right) {
-        var pivot = partition(arr, left, right);
-        quicksort_recursive(arr, left, pivot - 1);
-        quicksort_recursive(arr, pivot + 1, right);
+        var pivot = partition(arr, left, right, randomize);
+        quicksort_recursive(arr, left, pivot - 1, randomize);
+        quicksort_recursive(arr, pivot + 1, right, randomize);
     }
 }
 
-pub fn quicksort(arr: []i32) void {
-    quicksort_recursive(arr, 0, @intCast(i32, (arr.len - 1)));
+/// # QuickSort
+/// -------------------------------------------------------------------------
+/// Description:
+/// Sorts the array using the QuickSort algorithm. You can select 
+/// randomized **pivot** point by setting the `random_piv` argument to `true`
+/// -------------------------------------------------------------------------
+/// Arguments:
+/// > `arr`: Array to sort
+/// > `rand_piv`: randomized pivot select if `true`, else first element from 
+/// list
+/// -------------------------------------------------------------------------
+/// Return:
+/// > `void`
+///
+pub fn quicksort(arr: []i32, random_piv: bool) void {
+    if (arr.len < 1) {
+        return;
+    }
+    quicksort_recursive(arr, 0, @intCast(i32, (arr.len - 1)), random_piv);
 }
